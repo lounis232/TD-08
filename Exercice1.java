becfd0abb327dd47483032b06a55b941e667373a
Lounis Hadji <75098859+lounis232@users.noreply.github.com>
2021-12-15 13:58:06 +0100

    Create Exercice1.java

---
+++ Exercice1.java
@@ -0,0 +1,14 @@
+package td8;
+
+public class Exercice1 {
+
+    public static <E extends Comparable<? super E>> E myMax (E first, E ... others){
+        E max = first;
+        for (E o: others){
+            if (o.compareTo(max) > 0){
+                max = o;
+            }
+        }
+        return max;
+    }
+}
